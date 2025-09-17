e17f287f0cb9a281326c3f314b4da04e48949b86
Arthur Gehlen <1arthurgehlen@gmail.com>
2025-09-17 16:29:52 -0300

    Começo do menu

---
+++ src/components/MenuButton.jsx
@@ -0,0 +1,18 @@
+import "./MenuButton.css";
+
+import icon_checkmark from "../assets/images/icon-checkmark.svg";
+
+function MenuButton({ label, handle_click, is_selected, name }) {
+  return (
+    <button
+      className={`menu_btn ${is_selected ? "selected" : ""}`}
+      onClick={handle_click}
+      name={name}
+    >
+      {label}
+      {is_selected && <img src={icon_checkmark} alt="Check" />}
+    </button>
+  );
+}
+
+export default MenuButton;
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,20 +1,107 @@
-import "./Header.css"
+// Utils
+import { useState } from "react";
+import "./Header.css";
 
 // Images
-import logo from "../assets/images/logo.svg"
-import config_icon from "../assets/images/icon-units.svg"
-import dropdown_icon from "../assets/images/icon-dropdown.svg"
+import logo from "../assets/images/logo.svg";
+import config_icon from "../assets/images/icon-units.svg";
+import dropdown_icon from "../assets/images/icon-dropdown.svg";
+
+// Components
+import MenuButton from "../components/MenuButton";
 
 function Header() {
-  return <header id="header">
-    <img src={logo} alt="Logo" />
+  const [isMenuActive, setIsMenuActive] = useState(false);
+  const [unitsSelected, setUnitsSelected] = useState({
+    temperature: "Celsius",
+    wind_speed: "km",
+    precipitation: "mm",
+  });
 
-    <button className="unit_configs_btn">
-        <img src={config_icon} alt="Img" />
-        <span>Units</span>
-        <img src={dropdown_icon} alt="Drop" />
-    </button>
-  </header>;
+  const handle_unit_change = (e) => {
+    const name = e.target.name;
+
+    switch (name) {
+      case "Celsius":
+      case "Fahrenheit":
+        setUnitsSelected((prev) => ({ ...prev, temperature: name }));
+        break;
+      case "km":
+      case "mph":
+        setUnitsSelected((prev) => ({ ...prev, wind_speed: name }));
+        break;
+      case "mm":
+      case "in":
+        setUnitsSelected((prev) => ({ ...prev, precipitation: name }));
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <header id="header">
+      <img src={logo} alt="Logo" />
+
+      <div className="menu_wrapper">
+        <button
+          className="unit_configs_btn"
+          onClick={() => setIsMenuActive(!isMenuActive)}
+        >
+          <img src={config_icon} alt="Img" />
+          <span>Units</span>
+          <img src={dropdown_icon} alt="Drop" />
+        </button>
+        <div
+          id="menu"
+          style={isMenuActive ? { display: "flex" } : { display: "none" }}
+        >
+          <MenuButton label="Switch to Imperial" />
+          <p className="topic">Temperature</p>
+          <MenuButton
+            label="Celsius (°C)"
+            is_selected={unitsSelected["temperature"] == "Celsius"}
+            handle_click={handle_unit_change}
+            name="Celsius"
+          />
+          <MenuButton
+            label="Fahrenheit (°F)"
+            is_selected={unitsSelected["temperature"] == "Fahrenheit"}
+            handle_click={handle_unit_change}
+            name="Fahrenheit"
+          />
+          <div className="divider"></div>
+          <p className="topic">Wind Speed</p>
+          <MenuButton
+            label="km/h"
+            is_selected={unitsSelected["wind_speed"] == "km"}
+            handle_click={handle_unit_change}
+            name="km"
+          />
+          <MenuButton
+            label="mph"
+            is_selected={unitsSelected["wind_speed"] == "mph"}
+            handle_click={handle_unit_change}
+            name="mph"
+          />
+          <div className="divider"></div>
+          <p className="topic">Precipitation</p>
+          <MenuButton
+            label="Millimeters (mm)"
+            is_selected={unitsSelected["precipitation"] == "mm"}
+            handle_click={handle_unit_change}
+            name="mm"
+          />
+          <MenuButton
+            label="Inches (in)"
+            is_selected={unitsSelected["precipitation"] == "in"}
+            handle_click={handle_unit_change}
+            name="in"
+          />
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
